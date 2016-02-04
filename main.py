e70c60fd71c3a574f228841aab2877b0bfaafb75
phares <abungaphares@gmail.com>
2016-02-04 16:38:26 +0300

    zoho crm insert record

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ import webapp2
 import jinja2
 import os
 from google.appengine.api import mail
+
+import urllib
+
+import urllib2
 from jinja2 import Template
 
 
@@ -71,7 +75,7 @@ class Signup(webapp2.RequestHandler):
             message.body = """
                     Hi,
 
-                    The below client has field the online (isp website) contact form .
+                    The below client has filled the online (isp website) contact form .
 
                     Name: %s
                     Organization: %s
@@ -123,12 +127,83 @@ class Team(webapp2.RequestHandler):
     def get(self):
         template_values = {}
         template = jinja2_env.get_template('main/team.html')
-        self.response.out.write(template.render(template_values))       
+        self.response.out.write(template.render(template_values))
+
+class Crm(webapp2.RequestHandler):
+
+    def get(self):
+        module_name = 'Leads'
+        authtoken = '0f6d5b3e2cb345f1780860a34c154fc9'
+        params = {'authtoken':authtoken,'scope':'crmapi'}
+        final_URL = "https://crm.zoho.com/crm/private/xml/"+module_name+"/getRecords"
+        data = urllib.urlencode(params)
+        request = urllib2.Request(final_URL,data)
+        response = urllib2.urlopen(request)
+        xml_response = response.read()
+        #print xml_response
+
+        template_values = {
+            'xml_response':xml_response,
+        }
+        template = jinja2_env.get_template('main/xml_response.html')
+        self.response.out.write(template.render(template_values))
+
+class Crm3(webapp2.RequestHandler):
+
+    def get(self):
+
+        authtoken = '0f6d5b3e2cb345f1780860a34c154fc9'
+
+        params = {'authtoken':authtoken,'scope':'crmapi','xmlData':'<Leads>'
+                                                                       '<row no="1">'
+                                                                           '<FL val="Lead Source">internal (dev test)</FL>'                                                                           '<FL val="Closing Date">2014-06-28</FL>'
+                                                                           '<FL val="Company">Intellisoftplus Solutions</FL>'
+                                                                           '<FL val="First Name">Phares</FL>'
+                                                                            '<FL val="Last Name">Machira</FL>'
+                                                                            '<FL val="Email">pmachira@intellisoftplus.com.com</FL>'
+                                                                            '<FL val="Phone">0719301140</FL>'
+                                                                            '<FL val="City">Nairobi</FL>'
+                                                                            '<FL val="State">Upperhill</FL>'
+                                                                            '<FL val="Country">Kenya</FL>'
+                                                                            '<FL val="Zip Code">20100</FL>'
+                                                                       '</row>'
+                                                                   '</Leads>'
+                  }
+
+        final_URL = "https://crm.zoho.com/crm/private/xml/Leads/insertRecords"
+
+        data = urllib.urlencode(params)
+
+        print data
+
+        request = urllib2.Request(final_URL,data)
+
+        response = urllib2.urlopen(request)
+
+        xml_response = response.read()
+
+        print xml_response
+
+        template_values = {
+            'xml_response':xml_response,
+        }
+        template = jinja2_env.get_template('main/xml_response3.html')
+        self.response.out.write(template.render(template_values))
+
+class Crm2(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        template = jinja2_env.get_template('main/xml.xml')
+        self.response.out.write(template.render(template_values))
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/signup', Signup),
     ('/services', Services),
     ('/contactus', ContactUs),
-    ('/team', Team)
+    ('/team', Team),
+    ('/crm', Crm),
+    ('/crm2', Crm2),
+    ('/crm3', Crm3)
 ], debug=True)
